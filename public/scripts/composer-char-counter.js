c1fb9b5a0e2695f611abd33c1533d1292c2285a3
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-09 21:48:49 +0000

    added css stylesheet for tweets and refactored character counting code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
   console.log("Document is Ready!")
 });
 
-// $('.new-tweet textarea').css('border', '4px solid red')
 $('.new-tweet textarea').bind("keyup", function (event) {
   let textbox = $(this).val();
-  let counter = $(this).parent().children('.counter');
+  let counter = $(this).siblings('.counter');
   counter.text(140 - textbox.length)
   if (textbox.length > 140) {
     counter.css('color', 'red');
