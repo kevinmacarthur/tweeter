c861a308736d95d25b82b6cddcaceb7409f0d4c3
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-10 17:36:45 +0000

    added fetching tweets after posting new tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,11 +32,31 @@ function createTweetElement(element) {
 
 
 function renderTweets (data) {
+  $('#tweets-container').empty()
   for (var tweet of data) {
     var $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
+    $('#tweets-container').prepend($tweet);    //places tweet at top instead of append
   }
 }
+
+
+function postTweet(callback){
+  var $form = $('#tweet-form');
+  $form.on('submit', function (event) {
+    event.preventDefault();
+    let newTweet = $(this).serialize();
+    let counter = Number($(this).children('.counter').text());
+    if (counter === 140) {
+      alert("Error: Tweet is Blank")
+    } else if (counter < 0) {
+      alert("Error: Exceeded Character Count Limit")
+    } else {
+      $.post('/tweets/', newTweet).done(callback());
+      $form.trigger("reset");
+      $(this).children('.counter').text(140);
+    }
+  });
+};
 
 $(document).ready(function() {
 
@@ -44,16 +64,7 @@ $(document).ready(function() {
     $.getJSON('/tweets/').done(renderTweets);
   }
 
-  postTweet();
+  postTweet(loadTweets);
   loadTweets();
 })
-
-function postTweet(){
-  var $form = $('#tweet-form');
-  $form.on('submit', function (event) {
-    event.preventDefault();
-    let newTweet = $(this).serialize();
-    $.post('/tweets/', newTweet)
-  });
-};
 
